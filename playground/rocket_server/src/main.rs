bcc869ea5a08dbb719df23b34f23715a360551fb
omershlo <omer.shlomovits@gmail.com>
2019-01-20 14:20:33 +0200

    session id

--- playground/rocket_server/src/main.rs
+++ playground/rocket_server/src/main.rs
@@ -4,6 +4,7 @@
 
 extern crate rocket_contrib;
 extern crate reqwest;
+extern crate uuid;
 
 #[macro_use]
 extern crate serde_derive;
@@ -18,16 +19,20 @@ use rocket_contrib::json::Json;
 use std::sync::Mutex;
 use std::sync::RwLock;
 use rocket::config::{Config, Environment};
+use uuid::Uuid;
+
+const PARTIES: u32 = 3;
 
 #[derive(Hash)]
 #[derive(PartialEq, Eq, Clone, Debug, Serialize, Deserialize)]
 pub struct TupleKey {
     pub first: String,
-    pub second: String
+    pub second: String,
+    pub third: String,
 }
 impl TupleKey {
-    fn new(first: String, second: String) -> TupleKey {
-        return TupleKey {first, second };
+    fn new(first: String, second: String, third: String) -> TupleKey {
+        return TupleKey {first, second, third };
     }
 }
 fn pr<T : std::fmt::Debug + ?Sized>(x: &String) {
@@ -35,9 +40,16 @@ fn pr<T : std::fmt::Debug + ?Sized>(x: &String) {
 }
 impl fmt::Display for TupleKey {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.first, self.second)
+        write!(f, "({}, {}, {})", self.first, self.second,  self.third)
     }
 }
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct PartySignup {
+    pub number: u32,
+    pub uuid: String,
+}
+
 
 #[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
 pub struct Index{
@@ -79,17 +91,79 @@ fn set(
     hm.insert(entry.key.clone(), entry.value.clone());
     Json(Ok(()))
 }
+
+#[post("/signup", format = "json")]
+fn signup(
+    db_mtx: State<RwLock<HashMap<TupleKey,String>>>,
+) -> Json<Result<PartySignup,()>> {
+    let key  = TupleKey{
+        first: "signup".to_string(),
+        second: "".to_string(),
+        third: "".to_string(),
+    };
+    let mut hm = db_mtx.read().unwrap();
+    match hm.get(&key){
+        Some(value) => {
+            let party_i_minus1_signup : PartySignup = serde_json::from_str(&value).unwrap();
+            if party_i_minus1_signup.number < PARTIES {
+                let party_num = party_i_minus1_signup.number + 1;
+                let party_signup = PartySignup {
+                    number: party_num.clone(),
+                    uuid: party_i_minus1_signup.uuid,
+                };
+                // update state:
+                drop(hm);
+                let mut hm = db_mtx.write().unwrap();
+                hm.insert(key, serde_json::to_string(&party_signup).unwrap());
+                return Json(Ok(party_signup))
+            }
+        },
+        None => {},
+    }
+    // start new session
+
+    let uuid = Uuid::new_v4().to_string();
+    let party1 = 1;
+    let party_signup = PartySignup {
+        number: party1,
+        uuid,
+    };
+
+    // update state:
+    drop(hm);
+    let mut hm = db_mtx.write().unwrap();
+    hm.insert(key, serde_json::to_string(&party_signup).unwrap());
+
+    Json(Ok(party_signup))
+
+}
+
+/*
+fn start_new_session(key: TupleKey, db_mtx: State<RwLock<HashMap<TupleKey,String>>>) -> Json<Result<PartySignup,()>> {
+
+    let uuid = Uuid::new_v4().to_string();
+    let party1 = 1;
+    let party_signup = PartySignup {
+        number: party1,
+        uuid,
+    };
+    // update state:
+    let mut hm = db_mtx.write().unwrap();
+    hm.insert(key, serde_json::to_string(&party_signup).unwrap());
+    Json(Ok(party_signup))
+}
+*/
 //refcell, arc
 
 fn main() {
 
 
     let mut my_config = Config::development();
-    my_config.set_port(18001);
+   // my_config.set_port(18001);
     let db : HashMap<TupleKey, String> = HashMap::new();
     let db_mtx = RwLock::new(db);
     //rocket::custom(my_config).mount("/", routes![get, set]).manage(db_mtx).launch();
-    rocket::ignite().mount("/", routes![get, set]).manage(db_mtx).launch();
+    rocket::ignite().mount("/", routes![get, set, signup]).manage(db_mtx).launch();
 
 }
 
@@ -106,6 +180,7 @@ pub mod tests {
         let key  = TupleKey{
             first: "omer".to_string(),
             second: "shlomovits".to_string(),
+            third: "".to_string(),
         };
         let entry = Entry{
             key: key.clone(),
