bdc8ada707194b6d6bf7b708ed411cc15c6042ed
Avi <kozokinavi@gmail.com>
2019-04-06 16:55:31 +0300

    changed relay message delimiter

--- RelayServer/relay-server/common/src/common.rs
+++ RelayServer/relay-server/common/src/common.rs
@@ -1,7 +1,7 @@
 pub static CANT_REGISTER_RESPONSE:&str = "Can't register peer";
 pub static RELAY_ERROR_RESPONSE:&str ="Can't relay message";
 
-pub static RELAY_MESSAGE_DELIMITER:&str = ":";
+pub static RELAY_MESSAGE_DELIMITER:&str = ":::";
 pub static PK_MESSAGE_PREFIX:&str = "PUBLIC_KEY";
 pub static COMMITMENT_MESSAGE_PREFIX:&str = "COMMITMENT";
 pub static R_KEY_MESSAGE_PREFIX:&str = "R_KEY";
