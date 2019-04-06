b475250579f6478335ebbf535ea2b4ec074a324c
Avi <kozokinavi@gmail.com>
2019-04-06 16:41:55 +0300

    updated two_party_eddsa_client

--- RelayServer/relay-server/examples/two_party_eddsa_client.rs
+++ RelayServer/relay-server/examples/two_party_eddsa_client.rs
@@ -131,36 +131,54 @@ enum RELAY_MESSAGE_TYPE {
     SIGNATURE(String),  //  S_j
 }
 
+
 struct PeerData{
     /// data structure that holds the relevant data of the peers.
     /// In our case:
     /// pks: all the public keys of the peers
     /// TODO
     /// TODO
-    pks: Vec<String>,
-    commitments: Vec<String>,
-    r_s: Vec<String>,
-    sigs: Vec<String>,
+    pub pks: Vec<String>,
+    pub commitments: Vec<String>,
+    pub r_s: Vec<String>,
+    pub sigs: Vec<String>,
 }
+
+impl PeerData {
+    pub fn new() -> PeerData{
+        PeerData {
+            pks: Vec::new(),
+            commitments: Vec::new(),
+            r_s: Vec::new(),
+            sigs: Vec::new(),
+        }
+
+    }
+}
+
+
 
 fn resolve_relay_message_type(msg: &RelayMessage) -> RELAY_MESSAGE_TYPE {
     unimplemented!("");
     let msg_payload = msg.message.clone();
 
     let split_msg:Vec<&str> = msg_payload.split(RELAY_MESSAGE_DELIMITER).collect();
-    let msg_prefix = &split_msg[0];
+    let msg_prefix = split_msg[0];
+    let msg_payload = String::from( split_msg[1].clone());
     match msg_prefix {
-        PK_MESSAGE_PREFIX => {
-            return RELAY_MESSAGE_TYPE::PUBLIC_KEY(String::from( split_msg[1].clone()));
+        String::from(PK_MESSAGE_PREFIX)=> {
+            return RELAY_MESSAGE_TYPE::PUBLIC_KEY(msg_payload);
         },
-        COMMITMENT_MESSAGE_PREFIX => {
+        String::from(COMMITMENT_MESSAGE_PREFIX) => {
+            return RELAY_MESSAGE_TYPE::COMMITMENT(msg_payload);
             unimplemented!()
         },
-        R_KEY_MESSAGE_PREFIX => {
-            unimplemented!()
+        String::from(R_KEY_MESSAGE_PREFIX ) => {
+            return RELAY_MESSAGE_TYPE::R_MESSAGE(msg_payload);
+
         },
-        SIGNATURE_MESSAGE_PREFIX => {
-            unimplemented!()
+        String::from(SIGNATURE_MESSAGE_PREFIX)=> {
+            return RELAY_MESSAGE_TYPE::SIGNATURE(msg_payload);
         },
         _ => panic!("Unknown relay message prefix")
     }
@@ -189,6 +207,7 @@ fn main() {
 
 
     let mut session = ProtocolSession::new(PROTOCOL_IDENTIFIER_ARG, PROTOCOL_CAPACITY_ARG);
+    let mut peer_data = PeerData::new();
 
     let client = _tcp.and_then(|stream| {
         println!("sending register message");
@@ -200,31 +219,29 @@ fn main() {
         let mut msg = ClientMessage::new();
         let register_msg = msg.register(session.protocol_id.clone(), session.capacity.clone());
 
-        //let mut session = session.clone();
+        // let mut session = session.clone();
         // send register message to server
-	let send_ = framed_stream.send(msg);
-	//let session = session.clone();
-            send_.and_then(|stream| {
-		//let session = session.clone();
-                let (tx, rx) = stream.split();
-                let client = rx.and_then(|msg| {
-                    println!("Got message from server: {:?}", msg);
-                    let msg_type = resolve_msg_type(msg.clone());
-                    match msg_type {
-                        ServerMessageType::Response =>{
-                            let server_response = msg.response.unwrap();
-                            match server_response {
-                                ServerResponse::Register(peer_id) => {
-                                    println!("Peer identifier: {}",peer_id);
-                                    // Set the session parameters
-                                    session.peer_id.replace(peer_id);
-                                    session.set_bc_dests();
-                                    session.next_step();
+	    let send_ = framed_stream.send(msg);
+        send_.and_then(|stream| {
+            let (tx, rx) = stream.split();
+            let client = rx.and_then(|msg| {
+                println!("Got message from server: {:?}", msg);
+                let msg_type = resolve_msg_type(msg.clone());
+                match msg_type {
+                    ServerMessageType::Response =>{
+                        let server_response = msg.response.unwrap();
+                        match server_response {
+                            ServerResponse::Register(peer_id) => {
+                                println!("Peer identifier: {}",peer_id);
+                                // Set the session parameters
+                                session.peer_id.replace(peer_id);
+                                session.set_bc_dests();
+                                session.next_step();
 
-                                    //after register, generate signing key
-                                    let key = KeyPair::create();
-                                    let pk/*:Ed25519Point */= key.public_key;
-                                    let message =  serde_json::to_string(&pk).expect("Failed in serialization");key.public_key;
+                                //after register, generate signing key
+                                let key = KeyPair::create();
+                                let pk/*:Ed25519Point */= key.public_key;
+                                let message =  serde_json::to_string(&pk).expect("Failed in serialization");key.public_key;
 
 //                                    let (ephemeral_key, sign_first_message, sign_second_message) =
 //                                        Signature::create_ephemeral_key_and_commit(&key, &message);
@@ -232,76 +249,77 @@ fn main() {
 //                                    let commitment = &sign_first_message.commitment.clone();
 //                                    println!("sending commitment");
 
-                                    // create relay message
-                                    let mut client_message= ClientMessage::new();
-                                    let mut relay_message = RelayMessage::new(peer_id, session.protocol_id.clone());
-                                    let mut to: Vec<u32> = session.bc_dests.clone();
+                                // create relay message
+                                let mut client_message= ClientMessage::new();
+                                let mut relay_message = RelayMessage::new(peer_id, session.protocol_id.clone());
+                                let mut to: Vec<u32> = session.bc_dests.clone();
 
-                                    // wait a little so we can spawn the second client
-                                    let wait_time = time::Duration::from_millis(5000);
-                                    thread::sleep(wait_time);
+                                // wait a little so we can spawn the second client
+                                let wait_time = time::Duration::from_millis(5000);
+                                thread::sleep(wait_time);
 
-                                    relay_message.set_message_params(0, to, generate_pk_message_payload(&message));
-                                    client_message.relay_message = Some(relay_message.clone());
-                                    return Ok(client_message);
-                                },
-                                ServerResponse::ErrorResponse(err_msg) => {
-                                    println!("got error response");
-                                    return Ok(ClientMessage::new());
-                                },
-                                ServerResponse::GeneralResponse(msg) => {
-                                    unimplemented!()
-                                },
-                                ServerResponse::NoResponse => {
-                                  unimplemented!()
-                                },
-                                _ => panic!("failed to register")
-                            }
-                        }
-                        ServerMessageType::RelayMessage => {
-                            println!("Got new relay message");
-                            println!("{:?}", );
-                            // parse relay message
-                            // (if we got here this means we are registered and
-                            // the client sent the private key)
-
-                            // so at the first step we are expecting the pks from all other peers
-                            let relay_msg = msg.relay_message.unwrap();
-                            let msg_type = resolve_relay_message_type(&rel);
-                            match msg_type {
-                                // for each type
-                                // check if received data from all peers,
-                                // if so do the next step,
-                                // if not send empty message (means we are still waiting)
-                                RELAY_MESSAGE_TYPE::PUBLIC_KEY(pk) => {
-                                    println!("Got public key: {:}", pk);
-                                },
-                                RELAY_MESSAGE_TYPE::COMMITMENT(t) => {
-                                    unimplemented!()
-                                },
-                                RELAY_MESSAGE_TYPE::R_MESSAGE(r) => {
-                                    unimplemented!()
-                                },
-                                RELAY_MESSAGE_TYPE::SIGNATURE(s) => {
-                                    unimplemented!()
-                                }
-                                _ => panic!("Unknown relay message type")
-                            }
-                            Ok(ClientMessage::new())
-                        },
-                        ServerMessageType::Abort => {
-                            println!("Got abort message");
-                            //Ok(MessageProcessResult::NoMessage)
-                            Ok(ClientMessage::new())
-                        },
-                        ServerMessageType::Undefined => {
-                            Ok(ClientMessage::new())
-                            //panic!("Got undefined message: {:?}",msg);
+                                relay_message.set_message_params(0, to, generate_pk_message_payload(&message));
+                                client_message.relay_message = Some(relay_message.clone());
+                                return Ok(client_message);
+                            },
+                            ServerResponse::ErrorResponse(err_msg) => {
+                                println!("got error response");
+                                return Ok(ClientMessage::new());
+                            },
+                            ServerResponse::GeneralResponse(msg) => {
+                                unimplemented!()
+                            },
+                            ServerResponse::NoResponse => {
+                              unimplemented!()
+                            },
+                            _ => panic!("failed to register")
                         }
                     }
-                }).forward(tx);
-                client
-            })
+                    ServerMessageType::RelayMessage => {
+                        println!("Got new relay message");
+                        println!("{:?}", );
+                        // parse relay message
+                        // (if we got here this means we are registered and
+                        // the client sent the private key)
+
+                        // so at the first step we are expecting the pks from all other peers
+                        let relay_msg = msg.relay_message.unwrap();
+                        let msg_type = resolve_relay_message_type(&rel);
+                        match msg_type {
+                            // for each type
+                            // check if received data from all peers,
+                            // if so do the next step,
+                            // if not send empty message (means we are still waiting)
+                            RELAY_MESSAGE_TYPE::PUBLIC_KEY(pk) => {
+                                println!("Got public key: {:}", pk);
+                                peer_data.pks.push(pk);
+                            },
+                            RELAY_MESSAGE_TYPE::COMMITMENT(t) => {
+                                unimplemented!()
+                            },
+                            RELAY_MESSAGE_TYPE::R_MESSAGE(r) => {
+                                unimplemented!()
+                            },
+                            RELAY_MESSAGE_TYPE::SIGNATURE(s) => {
+                                unimplemented!()
+                            }
+                            _ => panic!("Unknown relay message type")
+                        }
+                        Ok(ClientMessage::new())
+                    },
+                    ServerMessageType::Abort => {
+                        println!("Got abort message");
+                        //Ok(MessageProcessResult::NoMessage)
+                        Ok(ClientMessage::new())
+                    },
+                    ServerMessageType::Undefined => {
+                        Ok(ClientMessage::new())
+                        //panic!("Got undefined message: {:?}",msg);
+                    }
+                }
+            }).forward(tx);
+            client
+        })
     })
         .map_err(|err| {
             // All tasks must have an `Error` type of `()`. This forces error
