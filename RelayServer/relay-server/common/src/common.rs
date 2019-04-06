0ebfcd1956c03a8a0375304159faf64def8e7199
Avi <kozokinavi@gmail.com>
2019-04-06 18:40:09 +0300

    fixes common.rs

--- RelayServer/relay-server/common/src/common.rs
+++ RelayServer/relay-server/common/src/common.rs
@@ -3,7 +3,7 @@
 // Error responses
 pub static CANT_REGISTER_RESPONSE:&str = "Can't register peer";
 pub static RELAY_ERROR_RESPONSE:&str = "Can't relay message";
-pub static STATE_NOT_INITIALIZED:&str = "Relay sessions state is not initialized"
+pub static STATE_NOT_INITIALIZED:&str = "Relay sessions state is not initialized";
 pub static RELAY_MESSAGE_DELIMITER:&str = ":::";
 pub static NOT_YOUR_TURN:&str = "Not this peers turn";
 
