8254dbde381faca17845cc82be117d27dfbed140
Avi <kozokinavi@gmail.com>
2019-04-17 19:51:42 +0300

    fixes for client

--- RelayServer/relay-server/examples/two_party_eddsa_client.rs
+++ RelayServer/relay-server/examples/two_party_eddsa_client.rs
@@ -85,7 +85,7 @@ impl EddsaPeer{
         let mut Ri:Vec<GE> = Vec::new();
         for (peer_id, r) in self.r_s {
             let r_sloce:&str = &r[..];
-            let _r:SignSecondMsg = serde_json::from_str(r_slice).unwrap_or_else(|| {panic!("serialization error")});
+            let _r:SignSecondMsg = serde_json::from_str(r_slice).unwrap_or_else(|e| {panic!("serialization error")});
             Ri.push(_r.R.clone());
         }
         let r_tot= Signature::get_R_tot(Ri);
@@ -153,7 +153,7 @@ impl EddsaPeer {
     }
 
     pub fn update_data_step_2(&mut self, from: PeerIdentifier, payload: MessagePayload) {
-        let payload_type = self.resolve_payload_type(&payload);
+        let payload_type = EddsaPeer::resolve_payload_type(&payload);
         match payload_type {
             MessagePayloadType::R_MESSAGE(r) => {
                 self.add_r(from, r);
@@ -163,7 +163,7 @@ impl EddsaPeer {
     }
 
     pub fn update_data_step_3(&mut self, from: PeerIdentifier, payload: MessagePayload) {
-        let payload_type = self.resolve_payload_type(&payload);
+        let payload_type = EddsaPeer::resolve_payload_type(&payload);
         match payload_type {
             MessagePayloadType::SIGNATURE(s) => {
                 self.add_sig(from, s);
@@ -208,9 +208,9 @@ impl EddsaPeer{
         // (this implicitly means each party also calculates ephemeral key
         // on this step)
         // round 1: send commitments to ephemeral public keys
-        let k = &self.client_key.public_key;
+        let k = &self.client_key;
         let (ephemeral_key, sign_first_message, sign_second_message) =
-            Signature::create_ephemeral_key_and_commit(&k, &self.message);
+            Signature::create_ephemeral_key_and_commit(k, &self.message);
 
         //let commitment = sign_first_message.commitment;
         // save the commitment
@@ -252,7 +252,7 @@ impl EddsaPeer{
         let k = Signature::k(&R_tot, &self.agg_key.unwrap_or_else(||{panic!("No AggKey")}).apk, &self.message);
         let peer_id = self.peer_id.clone().into_inner();
         let r = self.r_s.get(&peer_id).unwrap_or_else(||{panic!("Client has No R ")}).clone();
-        let _r: SignSecondMsg = serde_json::from_str(&r).unwrap_or_else(|| {panic!("failed to deserialize R")});
+        let _r: SignSecondMsg = serde_json::from_str(&r).unwrap_or_else(|e| {panic!("failed to deserialize R")});
         let key = &self.client_key;
         // sign
         let ephemeral_key = self.ephemeral_key.unwrap_or_else(||{panic!("No ephemeral key")});
@@ -531,7 +531,7 @@ impl<T: Peer> ProtocolSession<T> {
 
         let mut client_message =  ClientMessage::new();
 
-        relay_message.set_message_params(to,payload);
+        relay_message.set_message_params(to,String::from(payload));
         client_message.relay_message = Some(relay_message);
         client_message
     }
