42d38474168f286e1e0c69edef37bc2ed8cb05b3
Alex Manuskin <amanusk@zengocorp.com>
2019-10-17 12:11:28 +0000

    Use executables for cluster

--- RelayProofsOfConcept/EddsaTendermintServer/tools/kg-demo.sh
+++ RelayProofsOfConcept/EddsaTendermintServer/tools/kg-demo.sh
@@ -7,11 +7,14 @@ rm keys??
 
 n=5
 
+cargo build --all
+
 echo "keygen part"
 for i in $(seq 1 $n);
 do
     S=$(( ( RANDOM % 4 )  + 1 ))
     PORT="46${S}57"
     #PORT="46157"
-    cargo run -p mmpc-client --bin kg-client -- -I $i -C $n --proxy 127.0.0.1:$PORT -v &
+    # cargo run -p mmpc-client --bin kg-client -- -I $i -C $n --proxy 127.0.0.1:$PORT -v &
+    ./target/debug/kg-client -I $i -C $n --proxy 127.0.0.1:$PORT &
 done
--- RelayProofsOfConcept/EddsaTendermintServer/tools/sign-demo.sh
+++ RelayProofsOfConcept/EddsaTendermintServer/tools/sign-demo.sh
@@ -13,5 +13,6 @@ do
     S=$(( ( RANDOM % 4 )  + 1 ))
     PORT="46${S}57"
     #PORT="46157"
-    cargo run -p mmpc-client --bin  sign-client -- -I $i -C $n -M "message" --proxy 127.0.0.1:$PORT &
+    # cargo run -p mmpc-client --bin  sign-client -- -I $i -C $n -M "message" --proxy 127.0.0.1:$PORT &
+    ./target/debug/sign-client -I $i -C $n -M "message" --proxy 127.0.0.1:$PORT &
 done
