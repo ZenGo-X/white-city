13040bf5b7825f56bbb09f228a0b9364294712a0
HAOYUatHZ <haoyu@protonmail.com>
2019-10-18 22:09:20 +0800

    config init

--- RelayProofsOfConcept/EddsaTendermintServer/tools/docker/start-tendermint-local-testnet.sh
+++ RelayProofsOfConcept/EddsaTendermintServer/tools/docker/start-tendermint-local-testnet.sh
@@ -1,3 +1,5 @@
+cp ./tools/docker/tendermint_Makefile $GOPATH/src/github.com/tendermint/tendermint/Makefile
+
 cd $GOPATH/src/github.com/tendermint/tendermint
 
 # Build the linux binary in ./build
@@ -5,3 +7,7 @@ make build-linux
 
 # (optionally) Build tendermint/localnode image
 make build-docker-localnode
+
+rm -rf ./build/node*
+
+make NODENUM=5 localnet-start
