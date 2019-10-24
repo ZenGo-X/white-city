89dd4f9f22c47176103eaad83ba5c2d6da083d0c
HAOYUatHZ <haoyu@protonmail.com>
2019-10-24 13:55:53 +0800

    lao ge

--- RelayProofsOfConcept/EddsaTendermintServer/tools/docker/start-tendermint-local-testnet.sh
+++ RelayProofsOfConcept/EddsaTendermintServer/tools/docker/start-tendermint-local-testnet.sh
@@ -1,8 +1,8 @@
 where_am_i=$(pwd)
 # echo "$where_am_i"
-# cd $where_am_i
 
-make server
+cargo build --all
+docker-compose build
 
 # ./tools/docker/gen-docker-compose-config.py $1 > ./tools/docker/docker-compose.yml
 
